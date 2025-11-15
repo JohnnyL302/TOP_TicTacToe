8ad619dfeded2a241e60386c1df3014ebd4d31bd
Johnny Liu <33532804+JohnnyL302@users.noreply.github.com>
2025-11-14 21:13:23 -0500

    Grid is clickable

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # TOP_TicTacToe
 The Odin Project's Tic Tac Toe Project
+
+Utilizes Function Factories and IIFE Module Pattern
--- script.js
+++ script.js
@@ -4,7 +4,6 @@ const gameboard = (() => {
     for (let i = 0; i < 9; i++) {
         gameboard.push(""); 
     }
-
     const render = () => {
         let boardHTML = "";  
         gameboard.forEach((square, index) => {
@@ -12,7 +11,6 @@ const gameboard = (() => {
         })
         document.querySelector("#game-display").innerHTML = boardHTML; 
     }
-
     return {
         render,
     }
@@ -21,11 +19,48 @@ const gameboard = (() => {
 // create gameController 
 // needs to create players 
 
-gameboard.render(); // starts the game
+const createPlayer = (name, mark) => {
+    return {
+        name, 
+        mark 
+    }
+}
+
+const gameController = (() => {
+    let players = []; 
+    let currPlayerIndex; 
+    let gameOve; 
+
+    const start = () => {
+        players = [
+            createPlayer(document.querySelector("#player1").value, "X"), 
+            createPlayer(document.querySelector("#player2").value, "O")
+        ]
+        currPlayerIndex = 0; 
+        gameOver = false; 
+
+        // make each sqaure clickable 
+        const squares = document.querySelectorAll(".square"); 
+        squares.forEach((square) => {
+            square.addEventListener("click", makeClick);
+        })
+    }
+
+    const makeClick = (event) => {
+        console.log(event.target.id.split("-")[1]); 
+    }
+
+    return {
+        start,
+        // makeClick // unsure if i need to return this 
+    }
+})(); 
+
+gameboard.render(); // makes the 3x3 grid show up 
 
 const startButton = document.querySelector("#start-button");  
 startButton.addEventListener("click", () => {
-    alert("Hello world");
+    gameController.start(); 
 }); 
 
  
